1879dc0d15871f997b050fdc503ffb5771b8c863
Kasacko0451 <ivan.bakotin1999@gmail.com>
2021-09-03 21:29:06 +0200

    Update registerstrategy.js

--- backend/passport/registerstrategy.js
+++ backend/passport/registerstrategy.js
@@ -4,7 +4,7 @@ const salt = bcrypt.genSaltSync(10)
 const pool = require("../db.js");
 
 const registerStrategy = new LocalStrategy({ passReqToCallback: true }, async (req, username, password, done) => {
-    pool.connect()
+    
     const encryptedpass = bcrypt.hashSync(password, salt)
     const res_user = await pool.query("SELECT id FROM users WHERE username = $1", [username])
     if (res_user.rows[0])  return done("User already exists", null)
